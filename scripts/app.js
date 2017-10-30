bcc3581538bf4dce187d881a3ab9adf9c61b7534
daniellogerstedt <daniel.logerstedt@gmail.com>
2017-10-30 13:07:44 -0700

    commit of random generator and number comparator functions for documentation

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // App Scripts
+
+// Declared Variables
+
+var previousFirst; //will be assigned first image number each round.
+var previousSecond; //second image.
+var previousThird; //third image.
+var imgArray = [];
+
+// Random Number Generator
+
+function getRandom() {
+  var generatedNumber = Math.floor(Math.random() * imgArray.length); //Image Array Length
+  while (generatedNumber === previousFirst || generatedNumber === previousSecond || generatedNumber === previousThird) {
+    generatedNumber = Math.floor(Math.random() * imgArray.length); //Image Array Length
+  }
+  return generatedNumber;
+}
+
+// Random Number Comparator
+
+function compareNumbers() {
+  var firstImageNumber = getRandom();
+  var secondImageNumber = getRandom();
+  var thirdImageNumber = getRandom();
+  while (secondImageNumber === firstImageNumber) {
+    secondImageNumber = getRandom();
+  }
+  while (thirdImageNumber === firstImageNumber || thirdImageNumber === secondImageNumber) {
+    thirdImageNumber = getRandom();
+  }
+}
