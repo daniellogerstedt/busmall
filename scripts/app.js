58c4184c5f1c41e508e0de7a6cea0af08fe4ed9a
daniellogerstedt <daniel.logerstedt@gmail.com>
2017-10-30 18:21:04 -0700

    random number selection finished

--- scripts/app.js
+++ scripts/app.js
@@ -4,10 +4,33 @@
 
 // Declared Variables
 
+var counter = 0;
+var firstImageNumber;
+var secondImageNumber;
+var thirdImageNumber;
 var previousFirst; //will be assigned first image number each round.
 var previousSecond; //second image.
 var previousThird; //third image.
 var imgArray = [];
+var pageArray = [];
+var imgNames = [
+  'bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'
+];
+
+// Object Constructor for Image Array
+
+function ImageBuilder (name) {
+  this.path = './assets/' + name + '.jpg';
+  this.name = name;
+  this.displayed = 0;
+  this.picked = 0;
+}
+
+// Instanciation of Image Objects
+
+for (var i = 0; i < imgNames.length; i++) {
+  imgArray.push(new ImageBuilder(imgNames[i]));
+}
 
 // Random Number Generator
 
@@ -15,6 +38,7 @@ function getRandom() {
   var generatedNumber = Math.floor(Math.random() * imgArray.length); //Image Array Length
   while (generatedNumber === previousFirst || generatedNumber === previousSecond || generatedNumber === previousThird) {
     generatedNumber = Math.floor(Math.random() * imgArray.length); //Image Array Length
+    console.log('caught previous number');
   }
   return generatedNumber;
 }
@@ -22,13 +46,44 @@ function getRandom() {
 // Random Number Comparator
 
 function compareNumbers() {
-  var firstImageNumber = getRandom();
-  var secondImageNumber = getRandom();
-  var thirdImageNumber = getRandom();
+  firstImageNumber = getRandom();
+  secondImageNumber = getRandom();
+  thirdImageNumber = getRandom();
+  // console.log('first number', firstImageNumber);
+  // console.log('second number', secondImageNumber);
+  // console.log('third number', thirdImageNumber);
+  console.log('Begin Second Number Comparator');
   while (secondImageNumber === firstImageNumber) {
+    console.log('generated number', secondImageNumber);
     secondImageNumber = getRandom();
+    console.log('regenerated second number', secondImageNumber);
   }
+  console.log('Begin Third Number Comparator');
   while (thirdImageNumber === firstImageNumber || thirdImageNumber === secondImageNumber) {
+    console.log('generated number', thirdImageNumber);
     thirdImageNumber = getRandom();
+    console.log('regenerated third number', thirdImageNumber);
+  }
+  previousFirst = firstImageNumber;
+  previousSecond = secondImageNumber;
+  previousThird = thirdImageNumber;
+}
+
+// Test Function for Random Number Generation
+
+function testRun(){
+
+  for (var r = 0; r < 5; r++) {
+    compareNumbers();
+
+    console.log('Run', r);
+
+    console.log('first number', firstImageNumber);
+    console.log('second number', secondImageNumber);
+    console.log('third number', thirdImageNumber);
+
+    console.log('Begin Next Run');
   }
 }
+
+testRun();
