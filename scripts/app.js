aeb773d19d65c5afd5f5c232c79092dd80c32adf
daniellogerstedt <daniel.logerstedt@gmail.com>
2017-10-30 23:25:18 -0700

    modified images and finalized generation of images onto page

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,6 @@ var previousFirst; //will be assigned first image number each round.
 var previousSecond; //second image.
 var previousThird; //third image.
 var imgArray = [];
-var pageArray = [];
 var imgNames = [
   'bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'
 ];
@@ -64,26 +63,116 @@ function compareNumbers() {
     thirdImageNumber = getRandom();
     console.log('regenerated third number', thirdImageNumber);
   }
-  previousFirst = firstImageNumber;
-  previousSecond = secondImageNumber;
-  previousThird = thirdImageNumber;
 }
 
 // Test Function for Random Number Generation
 
-function testRun(){
+// function testRun(){
+//
+//   for (var r = 0; r < 5; r++) {
+//     compareNumbers();
+//
+//     console.log('Run', r);
+//
+//     console.log('first number', firstImageNumber);
+//     console.log('second number', secondImageNumber);
+//     console.log('third number', thirdImageNumber);
+//
+//     console.log('Begin Next Run');
+//   }
+// }
 
-  for (var r = 0; r < 5; r++) {
-    compareNumbers();
+// testRun();
 
-    console.log('Run', r);
+// Event Listener Manipulation
 
-    console.log('first number', firstImageNumber);
-    console.log('second number', secondImageNumber);
-    console.log('third number', thirdImageNumber);
-
-    console.log('Begin Next Run');
+function editEventListeners(flag) { //true will add listeners and false will remove them.
+  if (counter !== 25 && flag === 'add') {
+    console.log('c not 0', counter != 0);
+    console.log('c', counter);
+    document.getElementById(imgArray[firstImageNumber].name).addEventListener('click', displayPage);
+    document.getElementById(imgArray[secondImageNumber].name).addEventListener('click', displayPage);
+    document.getElementById(imgArray[thirdImageNumber].name).addEventListener('click', displayPage);
+  } else if (counter === 25 && flag === 'add') {
+    document.getElementById('click').addEventListener('click', displayPage);
+    document.getElementById('an').addEventListener('click', displayPage);
+    document.getElementById('image').addEventListener('click', displayPage);
+  }
+  if (counter !== 0 && flag === 'remove') { //only removes event listeners if they are present.
+    console.log('c not 0', counter !== 0);
+    console.log('c', counter);
+    document.getElementById(imgArray[previousFirst].name).removeEventListener('click', displayPage);
+    document.getElementById(imgArray[previousSecond].name).removeEventListener('click', displayPage);
+    document.getElementById(imgArray[previousThird].name).removeEventListener('click', displayPage);
+  } else if (counter === 0 && flag === 'remove'){
+    document.getElementById('click').removeEventListener('click', displayPage);
+    document.getElementById('an').removeEventListener('click', displayPage);
+    document.getElementById('image').removeEventListener('click', displayPage);
   }
 }
 
-testRun();
+// function removeImages() {
+//   document.getElementById(imgArray[previousFirst].name).remove();
+//   document.getElementById(imgArray[previousSecond].name).remove();
+//   document.getElementById(imgArray[previousThird].name).remove();
+// }
+
+// Display Page Contents function
+function displayPage() {
+  console.log(this.id);
+  if (this.id !== 'click' && this.id !== 'an' && this.id !== 'image') {
+    for (var i = 0; i < imgArray.length; i++) {
+      if (this.id === imgArray[i].name) {
+        imgArray[i].picked++;
+        break;
+      }
+    }
+  }
+  if (counter === 0) {
+    compareNumbers();
+    imgArray[firstImageNumber].displayed++;
+    imgArray[secondImageNumber].displayed++;
+    imgArray[thirdImageNumber].displayed++;
+    editEventListeners('remove');
+    document.getElementById('click').src = imgArray[firstImageNumber].path;
+    document.getElementById('click').id = imgArray[firstImageNumber].name;
+    document.getElementById('an').src = imgArray[secondImageNumber].path;
+    document.getElementById('an').id = imgArray[secondImageNumber].name;
+    document.getElementById('image').src = imgArray[thirdImageNumber].path;
+    document.getElementById('image').id = imgArray[thirdImageNumber].name;
+    editEventListeners('add');
+    counter++;
+  } else if (counter < 25 && counter !== 0) {
+    compareNumbers();
+    imgArray[firstImageNumber].displayed++;
+    imgArray[secondImageNumber].displayed++;
+    imgArray[thirdImageNumber].displayed++;
+    editEventListeners('remove');
+    document.getElementById(imgArray[previousFirst].name).src = imgArray[firstImageNumber].path;
+    document.getElementById(imgArray[previousFirst].name).id = imgArray[firstImageNumber].name;
+    document.getElementById(imgArray[previousSecond].name).src = imgArray[secondImageNumber].path;
+    document.getElementById(imgArray[previousSecond].name).id = imgArray[secondImageNumber].name;
+    document.getElementById(imgArray[previousThird].name).src = imgArray[thirdImageNumber].path;
+    document.getElementById(imgArray[previousThird].name).id = imgArray[thirdImageNumber].name;
+    editEventListeners('add'); // adds new event listeners and removes old ones
+    counter++;
+  } else if (counter === 25) {
+    editEventListeners('remove');
+    document.getElementById(imgArray[previousFirst].name).src = './assets/blank.jpg';
+    document.getElementById(imgArray[previousFirst].name).id = 'click';
+    document.getElementById(imgArray[previousSecond].name).src = './assets/blank.jpg';
+    document.getElementById(imgArray[previousSecond].name).id = 'an';
+    document.getElementById(imgArray[previousThird].name).src = './assets/blank.jpg';
+    document.getElementById(imgArray[previousThird].name).id = 'image';
+    editEventListeners('add');
+    counter = 0;
+  }
+  previousFirst = firstImageNumber;
+  previousSecond = secondImageNumber;
+  previousThird = thirdImageNumber;
+}
+// Test Click function
+
+document.getElementById('click').addEventListener('click', displayPage);
+document.getElementById('an').addEventListener('click', displayPage);
+document.getElementById('image').addEventListener('click', displayPage);
